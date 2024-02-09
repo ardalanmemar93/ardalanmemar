705b178ce8f02b87bad45087d4d7980f26cc2a45
ardalanmemar93 <ardalanmemar@gmail.com>
2024-02-09 14:48:55 -0800

    fixed tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,6 +19,8 @@ const Navbar = () => {
             }}
           >
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer">Ardalan <span className="sm:block hidden">| Memarr</span>
+            </p>
         </a>
       </div>
     </nav>
