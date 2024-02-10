b547a79f88edfeae3e643502a6326f716edb660f
ardalanmemar93 <ardalanmemar@gmail.com>
2024-02-09 21:08:45 -0800

    working on the hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,25 +5,41 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
       <hemisphereLight intensity={0.25} groundColor="black" />
-      <pointLight intensity={10} />
+      <pointLight intensity={1} position={[1, 0.1, 1]} />
       <spotLight position={[2, 5, 5]} angle={1} penumbra={1} intensity={150} castShadow shadow-mapSize={1024} /> 
-      <primitive object={computer.scene} scale={0.6} position={[1.5, -2.55, -0.75]} rotation={[-0.01, -0.2, -0.1]} />
+      <primitive 
+      object={computer.scene}
+      scale={isMobile ? 0.5 : 0.6}
+      position={isMobile ? [0, -2, -1.6] : [1.5, -2.55, -0.75]}
+      rotation={[-0.01, -0.2, -0.1]} />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+      const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+      setIsMobile(mediaQuery.matches)
+      const handleMediaQueryChange = (e) => {setIsMobile(e.matches)}
+
+      mediaQuery.addEventListener('change', handleMediaQueryChange)
+      return () => mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }, [])
+
     return (
       <Canvas shadows camera={{position: [20, 3, 5], fov: 22}} gl={{preserveDrawingBuffer: true}} frameloop="demand">
         <Suspense fallback={<CanvasLoader />} >
           <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-          <Computers />
+          <Computers isMobile={isMobile} />
         </Suspense>
 
         <Preload all />
