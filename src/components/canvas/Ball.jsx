286da8ce855cc9af9f0c52c9be82ee078db5614f
ardalanmemar93 <ardalanmemar@gmail.com>
2024-02-10 13:51:40 -0800

    working on the ball canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, Float, useTexture, Decal } from '@react-three/drei'
 
-const Ball = () => {
+import CanvasLoader from '../Loader'
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgURL])
   return (
     <div>Ball</div>
+  )
+}
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas>
+      
+    </Canvas>
   )
 }
 
