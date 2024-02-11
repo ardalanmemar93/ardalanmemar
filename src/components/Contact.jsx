69a196b80fe98dfcaad5d3eb2323fc09abec5d9a
ardalanmemar93 <ardalanmemar@gmail.com>
2024-02-10 19:36:49 -0800

    working on earth model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,8 +40,15 @@ const Contact = () => {
             <span className="text-whit font-medium mb-4">Your Message</span>
             <textarea rows="7" name="message" value={form.message} onChange={handleChange} placeholder="What do you want to say?" className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
           </label>
+          <button type="submit" className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
+            {loading ? 'Sending...' : 'Send'}
+          </button>
 
         </form>
+      </motion.div>
+
+      <motion.div variants={slideIn('right', "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+        <EarthCanvas />
       </motion.div>
     </div>
   )
